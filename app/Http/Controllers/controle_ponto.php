2f0ab5ac3feaaa621ac25113fbc9a4cd9914d657
reynaldo <reynaldotadeu10@gmail.com>
2018-11-27 22:56:00 -0200

    rey

--- app/Http/Controllers/controle_ponto.php
+++ app/Http/Controllers/controle_ponto.php
@@ -3,10 +3,14 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
+use Carbon\Carbon;
+use Illuminate\Support\Facades\DB;
 class controle_ponto extends Controller
 {
     public function index(){
-        return view('controle_ponto.controle_ponto');
+        $data = Carbon::now('America/Sao_Paulo');
+    	$ponto = DB::table('ponto')->where('data', $data->toDateString())->get();
+        return view('controle_ponto.controle_ponto',['ponto' => $ponto ]);
     }
+
 }
--- config/database.php
+++ config/database.php
@@ -1,12 +1,11 @@
 <?php
 
-
+/*
 $url = parse_url(getenv("DATABASE_URL"));
 $host = $url["host"];
 $username = $url["user"];
 $password = $url["pass"];
-$database = substr($url["path"], 1);
-
+$database = substr($url["path"], 1);*/
 
 return [
 
@@ -21,7 +20,7 @@ return [
     |
     */
 
-    'default' => env('DB_CONNECTION', 'pgsql_producao'),
+    'default' => env('DB_CONNECTION', 'mysql'),
 
     /*
     |--------------------------------------------------------------------------
@@ -53,7 +52,7 @@ return [
             'port' => env('DB_PORT', '3306'),
             'database' => env('DB_DATABASE', 'quimictec'),
             'username' => env('DB_USERNAME', 'root'),
-            'password' => env('DB_PASSWORD', 'Smit@banco'),
+            'password' => env('DB_PASSWORD', ''),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
@@ -61,7 +60,7 @@ return [
             'prefix_indexes' => true,
             'strict' => true,
             'engine' => null,
-        ],
+        ],/*
 
         'pgsql' => [
             'driver' => 'pgsql',
@@ -86,7 +85,7 @@ return [
             'charset'  => 'utf8',
             'prefix'   => '',
             'schema'   => 'public',
-        ),
+        ),*/
         'sqlsrv' => [
             'driver' => 'sqlsrv',
             'host' => env('DB_HOST', 'localhost'),
