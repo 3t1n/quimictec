79b4c03e087247d72b7a863ccaa85b3917f00250
Henrique Mamede da Silva <44325877+HenryMameds@users.noreply.github.com>
2019-04-14 00:42:16 -0300

    Comentários

--- app/Http/Controllers/controle_ponto.php
+++ app/Http/Controllers/controle_ponto.php
@@ -9,7 +9,9 @@ class controle_ponto extends Controller
 {
     public function index(){
         $data = Carbon::now('America/Sao_Paulo');
+        //chamada da tablea ponto por data
     	$ponto = DB::table('ponto')->where('data', $data->toDateString())->get();
+        //mostra a pagina de controle de ponto
         return view('controle_ponto.controle_ponto',['ponto' => $ponto ]);
 
     }
